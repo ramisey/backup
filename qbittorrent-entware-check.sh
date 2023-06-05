d1047c19a528f8ad6ebb65790d64984c79466d99
eorendel <duyouman@gmail.com>
2023-06-05 09:36:27 +0800

    Update qbittorrent-entware-check.sh

--- qbittorrent-entware-check.sh
+++ qbittorrent-entware-check.sh
@@ -7,13 +7,13 @@ if [ -z "$processID" ]
 then
     echo "qbittorrent程序未运行, 重新启动中..."
     /opt/etc/init.d/S89qbittorrent start
-    echo $(ps -ef | grep qbittorrent | grep -v "grep")
+    #echo $(ps -ef | grep qbittorrent | grep -v "grep")
     if [ -z "ps -ef | grep qbittorrent | grep -v "grep"" ]
     then
        echo "qbittorrent程序启动失败..."
     else
        echo "qbittorrent程序启动成功!"
-       echo $(ps -ef | grep qbittorrent | grep -v "grep")
+       #echo $(ps -ef | grep qbittorrent | grep -v "grep")
     fi         
 else
     echo "qbittorrent程序正在运行中..."
